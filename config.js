3ce4290cf82e8cac26f53a1aa03d872da514fbb9
Chandra0123456 <128498659+Chandra0123456@users.noreply.github.com>
2023-05-21 16:14:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -37,8 +37,8 @@ global.mess = (type, m, options = {}) => {
 		owner: 'Perintah ini hanya dapat digunakan oleh Owner!',
 		group: 'Perintah ini hanya dapat digunakan di group!',
 		private: 'Perintah ini hanya dapat digunakan di private chat!',
-		admin: 'Perintah ini hanya dapat digunakan oleh admin group!',
-		botAdmin: 'Bot bukan admin, tidak dapat mengakses fitur tersebut',
+		admin: 'Perintah ini hanya dapat digunakan oleh admin group, lu tuh cuma member!!',
+		botAdmin: 'jadikan bot sebagai admin terlebih dahulu 🙏',
 		bot: 'Fitur ini hanya dapat diakses oleh Bot',
 		locked: 'Fitur ini telah dinonaktifkan!',
 		media: 'Reply media...',
@@ -57,14 +57,14 @@ global.mess = (type, m, options = {}) => {
 global.options = {
 	public: true,
 	URI: "mongodb+srv://hisoka:<pwmu>@hisoka.xtav1oz.mongodb.net/?retryWrites=true&w=majority",
-	owner: ["6288292024190", "6285815663170"],
+	owner: ["6282163403132", "6283182628110"],
 	pathCommand: 'commands'
 }
 global.Exif = {
-	packId: "https://instagram.com/cak_haho",
-	packName: `Sticker ini Dibuat Oleh :\nhttps://instagram.com/cak_haho\n\nPada :\n${Function.tanggal(new Date())} Pukul ${Function.jam(new Date())} WIB\n\nOleh :\nDika Ardnt.`,
-	packPublish: "Dika Ardnt.",
-	packEmail: "dika.ardianta7@yahoo.com",
+	packId: "https://instagram.com/cw",
+	packName: `Sticker ini Dibuat Oleh :\nhttps://instagram.com/cw\n\nPada :\n${Function.tanggal(new Date())} Pukul ${Function.jam(new Date())} WIB\n\nOleh :\nDika Ardnt.`,
+	packPublish: "CW",
+	packEmail: "chandssi00@gmail.com",
 	packWebsite: "https://instgaram.com/cak_haho",
 	androidApp: "https://play.google.com/store/apps/details?id=com.bitsmedia.android.muslimpro",
 	iOSApp: "https://apps.apple.com/id/app/muslim-pro-al-quran-adzan/id388389451?|=id",
@@ -73,7 +73,7 @@ global.Exif = {
 }
 global.session = {
 	Path: "session",
-	Name: "hisoka"
+	Name: "`Chaneedyou"
 }
 
 
